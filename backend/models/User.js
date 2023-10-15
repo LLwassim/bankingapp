9c64e825fdca209693fa4b57dfe9ee4e317d9841
LLwassim <lacorchy77@gmail.com>
2023-10-15 19:22:39 -0400

    Fixed Sequilize errors. need to make customer_id

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,22 +1,28 @@
-// models/User.js
 const { DataTypes } = require("sequelize");
-const sequelize = require("../database");
+const sequelize = require("../database.js");
 
-const User = sequelize.define("User", {
-  customer_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const User = sequelize.define(
+  "User",
+  {
+    customer_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    date_of_birth: DataTypes.DATE,
+    contact_number: DataTypes.STRING,
+    address: DataTypes.TEXT,
+    account_type: DataTypes.STRING,
+    created_at: DataTypes.DATE,
   },
-  first_name: DataTypes.STRING,
-  last_name: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
-  date_of_birth: DataTypes.DATE,
-  contact_number: DataTypes.STRING,
-  address: DataTypes.TEXT,
-  account_type: DataTypes.STRING,
-  created_at: DataTypes.DATE,
-});
+  {
+    tableName: "users", // Specify the table name here
+    timestamps: false,
+  }
+);
 
 module.exports = User;
